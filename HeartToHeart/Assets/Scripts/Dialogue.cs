5b8e4f6229075df59f7d1369c50a59a2018d200b
Maksym Yaremko <maksymyaremko0202@gmail.com>
2023-03-28 00:06:08 -0400

    Backgrounds can now change (with and without fading)

--- HeartToHeart/Assets/Scripts/Dialogue.cs
+++ HeartToHeart/Assets/Scripts/Dialogue.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using TMPro;
 using System.Linq;
 using System.IO;
@@ -16,6 +17,15 @@ public class Dialogue : MonoBehaviour
     // information for the data being read from the file
     private List<string> VNScript;
     private int index;
+
+    // object to fade between scenes
+    public Image fader;
+
+    // 0 - APARTMENT
+    // 1 - SUBWAY
+    // 2 - OFFICE
+    public List<Texture> backgrounds;
+    public GameObject currBg;
 
     // Start is called before the first frame update
     void Start()
@@ -58,6 +68,7 @@ public class Dialogue : MonoBehaviour
             return;
         }
 
+        // read the next line!
         if (VNScript[index].Contains("[Line]"))
         {
             // clear current text
@@ -65,6 +76,22 @@ public class Dialogue : MonoBehaviour
 
             string line = VNScript[index].Substring(7);
             StartCoroutine(Type(line));
+        }
+        else if (VNScript[index].Contains("[Background]"))
+        {
+            // do we need to fade?
+            if (VNScript[index].Contains("[F]"))
+            {
+                // change bg!
+                string bgName = VNScript[index].Substring(17);
+                ChangeBG(bgName, true);
+            }
+            else
+            {
+                // change bg!
+                string bgName = VNScript[index].Substring(13);
+                ChangeBG(bgName, false);
+            }
         }
         
     }
@@ -89,9 +116,53 @@ public class Dialogue : MonoBehaviour
 
         crawling = false;
     }
-
-    IEnumerator ChangeBG()
+    void ChangeBG(string bg, bool fade)
     {
-        yield return null;
+        float length = 0f;
+
+        if (fade)
+            length = 0.5f;
+
+        switch(bg)
+        {
+            case "Apartment":
+                StartCoroutine(ChangeBG(backgrounds[0], length));
+                break;
+            case "Subway":
+                StartCoroutine(ChangeBG(backgrounds[1], length));
+                break;
+            case "Office":
+                StartCoroutine(ChangeBG(backgrounds[2], length));
+                break;
+        }
+    }
+
+    IEnumerator ChangeBG(Texture bgChange, float duration)
+    {
+        float time = 0;
+
+        // fade to black
+        while (time < duration)
+        {
+            fader.color = new Color(0, 0, 0, (time / duration));
+            time += Time.deltaTime;
+
+            yield return null;
+        }
+        fader.color = new Color(0, 0, 0, 1);
+
+        // change bg texture
+        currBg.GetComponent<RawImage>().texture = bgChange;
+
+        // fade to transparent
+        time = 0;
+        while (time < duration)
+        {
+            fader.color = new Color(0, 0, 0, 1f - (time / duration));
+            time += Time.deltaTime;
+
+            yield return null;
+        }
+        fader.color = new Color(0, 0, 0, 0);
     }
 }
